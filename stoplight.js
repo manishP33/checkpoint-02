cbafa8f4a932e6942ec2096157fc4eca8560d2f1
manish pradhan <pradhanmanish33@gmail.com>
2016-09-19 10:32:06 -0400

    commit

--- stoplight.js
+++ stoplight.js
@@ -1,18 +1,26 @@
 
 
 $ ("#red").click(function(){
-
-    $("div#red.bulb.active").css("background", "#555");
+$("div#red.bulb.active").css("background", "#555");
+$("div#yellow.bulb").css("background", "#555");
+$("div#red.bulb.active").css("background", "red");
+  $("div#green.bulb").css("background", "#555");
 
 
 });
 
+
 $ ("#yellow").click(function(){
   $("div#yellow.bulb").css("background", "yellow");
+  $("div#red.bulb.active").css("background", "#555");
+    $("div#green.bulb").css("background", "#555");
+
 
 });
 
 $ ("#green").click(function(){
   $("div#green.bulb").css("background", "green");
+  $("div#yellow.bulb").css("background", "#555");
+  $("div#red.bulb.active").css("background", "#555");
 
 });
